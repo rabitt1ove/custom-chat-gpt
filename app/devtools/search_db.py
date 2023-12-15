7650c02dea1d9fe16d5253d0516cd2dfa86882b8
rabitt1ove <45308877+rabitt1ove@users.noreply.github.com>
2023-12-15 18:41:05 +0900

    Update search_db.py

--- app/devtools/search_db.py
+++ app/devtools/search_db.py
@@ -4,7 +4,7 @@ sys.path.append('/app')
 from client.qdrant_manager import QdrantManager
 
 # 類似度検索を実行
-query = "OCX"
+query = "検索ワード"
 qdrant_manager = QdrantManager()
 docs = qdrant_manager.qdrant.similarity_search(query=query, k=2)
 for i in docs:
